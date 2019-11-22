01a3c7b0b319e70a6d6ed655e8e4dfd5f4f0b16c
Sn0wCrack <snowcrack123@gmail.com>
2019-11-22 23:59:53 +1100

    Output a bit more in the e621 embed
    Also prevent embeding video and flash files

--- saucybot/sites/e621.py
+++ saucybot/sites/e621.py
@@ -15,17 +15,26 @@ class e621(Base):
     def process(self, match):
 
         for (e621_id) in match.groups():
-            response = requests.get(self.api_url.format(e621_id), headers={'User-Agent': 'SaucyBot/0.1.0'})
+            response = requests.get(self.api_url.format(e621_id), headers={
+                                    'User-Agent': 'SaucyBot/0.1.0'})
 
             if not response:
                 return None
 
             parsed_response = json.loads(response.text)
 
+            # Don't try and embed videos and flash files
+            if parsed_response['file_ext'] in ['webm', 'mp4', 'swf']: 
+                return None
+
             ret = {}
 
-            discord_embed = discord.Embed(title=parsed_response['artist'][0])
+            discord_embed = discord.Embed(title='#{}: {}'.format(
+                parsed_response['id'], parsed_response['artist'][0]), description=parsed_response['description'])
+            
             discord_embed.set_image(url=parsed_response['file_url'])
+
+            discord_embed.set_author(name=parsed_response['author'])
 
             ret['embed'] = discord_embed
 
