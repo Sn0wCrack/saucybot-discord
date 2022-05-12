d3bb98857d91a7f524c55349a5a0dfe06385f354
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2022-05-12 22:23:04 +1000

    implement stubs for the rest of the sites needed

---
+++ SaucyBot/Site/ExHentai.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+using Discord.WebSocket;
+using SaucyBot.Site.Response;
+
+namespace SaucyBot.Site;
+
+public class ExHentai : BaseSite
+{
+    public override string Identifier => "ExHentai";
+    
+    public override Task<ProcessResponse?> Process(Match match, SocketUserMessage? message = null)
+    {
+        throw new NotImplementedException();
+    }
+}
