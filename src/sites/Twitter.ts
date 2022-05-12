66029f7e2f2310abc333cb012b705ed98f90e75e
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2022-05-12 20:48:45 +1000

    fix: twitter embed happening for video when it doesn't need to

--- src/sites/Twitter.ts
+++ src/sites/Twitter.ts
@@ -65,7 +65,7 @@ class Twitter extends BaseSite {
         //  - The result is "sensitive" and it has a video, as Discord often fails to play these inline
 
         if (videoMedia && tweet.possibly_sensitive) {
-            return this.handleVideo(tweet, match[0], hasTwitterEmbed !== null);
+            return this.handleVideo(tweet, match[0], hasTwitterEmbed === null);
         }
 
         if (hasTwitterEmbed) {
