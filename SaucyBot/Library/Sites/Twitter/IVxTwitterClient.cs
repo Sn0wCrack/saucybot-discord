914e8c717907736dad6af707de7fa7c92c123cd3
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2023-08-14 20:03:04 +1000

    feat: add vxtwitter client
    unfortunately not quite as good as fxtwitter, but apparently is more reliable in most cases?

---
+++ SaucyBot/Library/Sites/Twitter/IVxTwitterClient.cs
@@ -0,0 +1,6 @@
+﻿namespace SaucyBot.Library.Sites.Twitter;
+
+public interface IVxTwitterClient
+{
+    public Task<VxTwitterResponse?> GetTweet(string name, string identifier);
+}
---
+++ SaucyBot/Library/Sites/Twitter/VxTwitterClient.cs
@@ -0,0 +1,75 @@
+﻿using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SaucyBot.Services;
+
+namespace SaucyBot.Library.Sites.Twitter;
+
+public class VxTwitterClient: IVxTwitterClient
+{
+    private const string BaseUrl = "https://api.vxtwitter.com";
+
+    private readonly ILogger<VxTwitterClient> _logger;
+    
+    private readonly ICacheManager _cache;
+    
+    private readonly HttpClient _client = new();
+
+    public VxTwitterClient(ILogger<VxTwitterClient> logger, ICacheManager cacheManager)
+    {
+        _logger = logger;
+        _cache = cacheManager;
+        
+        _client.DefaultRequestHeaders.UserAgent.Add(
+            new ProductInfoHeaderValue("SaucyBot", Assembly.GetEntryAssembly()?.GetName().Version?.ToString())    
+        );
+        
+        _client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json")
+        );
+    }
+    
+    public async Task<VxTwitterResponse?> GetTweet(string name, string identifier)
+    {
+        var response = await _cache.Remember(
+            $"vxtwitter.tweet_{name}_{identifier}",
+            async () => await _client.GetStringAsync($"{BaseUrl}/{name}/status/{identifier}")
+        );
+
+        return response is null ? null : JsonSerializer.Deserialize<VxTwitterResponse>(response);
+    }
+}
+
+
+#region Response Types
+
+public sealed record VxTwitterResponse(
+    [property: JsonPropertyName("tweetID")]
+    string Id,
+    [property: JsonPropertyName("date_epoch")]
+    long DateEpoch,
+    [property: JsonPropertyName("likes")]
+    int Likes,
+    [property: JsonPropertyName("replies")]
+    int Replies,
+    [property: JsonPropertyName("retweets")]
+    int Retweets,
+    [property: JsonPropertyName("text")]
+    string Text,
+    [property: JsonPropertyName("user_name")]
+    string UserName,
+    [property: JsonPropertyName("user_screen_name")]
+    string UserScreenName,
+    [property: JsonPropertyName("media_extended")]
+    List<VxTwitterMedia> Media
+ );
+
+public sealed record VxTwitterMedia(
+    [property: JsonPropertyName("type")]
+    string Type,
+    [property: JsonPropertyName("url")]
+    string Url
+);
+
+#endregion
